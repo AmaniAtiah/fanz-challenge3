878cbe1722ffc64b0f5bf7588247cc6672066ec7
Amaniatiah <amaniatiah@gmail.com>
2024-07-01 01:04:58 +0300

    done challenge3

--- maxmin.php
+++ maxmin.php
@@ -6,19 +6,20 @@
  Definition of a function called findMaxAndMin that takes an array of integers as a parameter and returns an associative array containing the maximum and minimum numbers in the array.
 */
 
- function findMaxAndMin($arr) {
+function findMaxAndMin($arr)
+{
     // Initializing the maximum and minimum numbers to the first number in the array.
-    $max = $arr[0]; 
+    $max = $arr[0];
     $min = $arr[0];
 
     // Looping through each number in the array.
-    foreach($arr as $number) {
+    foreach ($arr as $number) {
         // Checking if the current number is greater than the current maximum and updating the maximum if necessary.
-        if($number > $max) {
+        if ($number > $max) {
             $max = $number;
         }
         // Checking if the current number is less than the current minimum and updating the minimum if necessary.
-        if($number < $min) {
+        if ($number < $min) {
             $min = $number;
         }
     }
@@ -27,6 +28,13 @@
     return array('Max' => $max, 'Min' => $min);
 
 
- }
+    // Time complexity: O(n), where n is the number of elements in the array.
+    // This is because we need to iterate through each element in the array once to find the maximum and minimum numbers.
+
+
+
+}
+
+
 
 ?>
